3db5776268c7a4e3ae96145b530c492502655077
Dietrich Featherston <d@d2fn.com>
2024-05-30 22:26:10 -0500

    update configs

--- polybar/.config/polybar/launch_polybar.sh
+++ polybar/.config/polybar/launch_polybar.sh
@@ -1,7 +1,7 @@
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &
+    MONITOR=$m polybar --reload d2fn &
   done
 else
-  polybar --reload toph &
+  polybar --reload d2fn &
 fi
