79ac3469c2856f2144f13288af019124264f349d
Dietrich Featherston <dfeatherston@netflix.com>
2024-06-03 16:06:21 -0700

    nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,54 @@
-require("d2fn")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { 'nvim-telescope/telescope.nvim',
+	tag = '0.1.6',
+	dependencies = { 'nvim-lua/plenary.nvim' } },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    }
+  }
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+local telescope = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', telescope.find_files, {})
+vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
+
+local treesitter = require("nvim-treesitter.configs")
+treesitter.setup({
+  ensure_installed = { "c", "rust", "javascript", "typescript", "go", "lua", "java" },
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
+vim.keymap.set('n', '<leader>t', ':Neotree filesystem reveal left<CR>')
+
+-- require("d2fn")
 
