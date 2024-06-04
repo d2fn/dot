089e46f7a52412f1fad7aa02bfa143a56111e4cc
Dietrich Featherston <d@d2fn.com>
2024-06-04 10:19:42 -0500

    remove old stuff

--- nvim/.config/nvim/_after/plugin/packer.lua
+++
@@ -1,9 +0,0 @@
-local packer = require('packer')
-packer.util = require('packer.util')
-
-packer.init({
-	git = {
-		clone_timeout = 60,
-	},
-	-- log = { level = 'debug' }
-})
--- nvim/.config/nvim/_lua/d2fn/init.lua
+++
@@ -1,2 +0,0 @@
-require("d2fn.remap")
-
