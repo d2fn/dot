ae64547d6b45ece50c8e3879d6184a1caaae7ef3
Dietrich Featherston <d@d2fn.com>
2025-10-02 08:33:54 -0700

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,6 +10,8 @@
 			./hardware-configuration.nix
 		];
 
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
 	# Bootloader.
 	boot.loader.grub.enable = true;
 	boot.loader.grub.device = "/dev/vda";
