b55036a0f2eb67b1d0fd95d523b19109b4cef033
Dietrich Featherston <d@d2fn.com>
2025-10-15 13:15:08 -0500

    add zsa voyager udev rules for flashing keyboard layouts from the browser

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -85,14 +85,14 @@
 		#media-session.enable = true;
 	};
 
-	# Enable touchpad support (enabled default in most desktopManager).
-	# services.xserver.libinput.enable = true;
+  # 2) Declare the plugdev group and put your user in it
+  users.groups.plugdev = { };
 
 	# Define a user account. Don't forget to set a password with ‘passwd’.
 	users.users.d = {
 		isNormalUser = true;
 		description = "Dietrich Featherston";
-		extraGroups = [ "networkmanager" "wheel" ];
+		extraGroups = [ "networkmanager" "wheel" "plugdev" ];
 		packages = with pkgs; [
 		#	thunderbird
 		];
@@ -209,6 +209,37 @@
 		#xcbutilcursor  # libxcb-cursor.so.0, often needed by Qt’s xcb platform plugin
 
 	];
+
+	  # 1) Add the udev rules for ZSA devices
+  services.udev.extraRules = ''
+# Rules for Oryx web flashing and live training
+KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", GROUP="plugdev"
+KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="plugdev"
+
+# Legacy rules for live training over webusb (Not needed for firmware v21+)
+  # Rule for all ZSA keyboards
+  SUBSYSTEM=="usb", ATTR{idVendor}=="3297", GROUP="plugdev"
+  # Rule for the Moonlander
+  SUBSYSTEM=="usb", ATTR{idVendor}=="3297", ATTR{idProduct}=="1969", GROUP="plugdev"
+  # Rule for the Ergodox EZ
+  SUBSYSTEM=="usb", ATTR{idVendor}=="feed", ATTR{idProduct}=="1307", GROUP="plugdev"
+  # Rule for the Planck EZ
+  SUBSYSTEM=="usb", ATTR{idVendor}=="feed", ATTR{idProduct}=="6060", GROUP="plugdev"
+
+# Wally Flashing rules for the Ergodox EZ
+ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
+
+# Keymapp / Wally Flashing rules for the Moonlander and Planck EZ
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE:="0666", SYMLINK+="stm32_dfu"
+# Keymapp Flashing rules for the Voyager
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0666", SYMLINK+="ignition_dfu"
+  '';
+
+  # Optional: some boards enumerate as ttyACM/USB serial; this helps avoid surprise perms
+  # users.users.yourusername.extraGroups = [ "plugdev" "dialout" "uucp" ];
 
 	# Some programs need SUID wrappers, can be configured further or are
 	# started in user sessions.
