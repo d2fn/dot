935aa7e13000eb2181e21a4ba78a2f7a45240aba
Dietrich Featherston <d@d2fn.com>
2025-10-16 10:50:41 -0500

    configure telescope to grep within .config subdirs for quicker dotfile editing

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,2 +1,21 @@
-require('telescope').setup{}
+require('telescope').setup({
+	defaults = {
+		file_ignore_patterns = { "%.git/" },
+	},
+	pickers = {
+
+		find_files = {
+			hidden = true,
+			no_ignore = true,
+			follow = true,
+		},
+
+		live_grep = {
+			hidden = true,
+			no_ignore = true,
+			follow = true,
+		},
+
+	}
+})
 
