59eda37382135e27e251dc81b8f261a762455436
Dietrich Featherston <d@d2fn.com>
2025-11-02 00:47:08 -0500

    launcher for chome

---
+++ hypr/.config/hypr/scripts/chrome.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+google-chrome-stable \
+  --user-data-dir="$HOME/.config/nflx-google-chrome" \
+  --profile-directory="dfeatherston" \
+  --no-first-run --no-default-browser-check &
+
