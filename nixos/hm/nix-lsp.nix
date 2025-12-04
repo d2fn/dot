b4eeede80463394910de6879490873c3d4640391
Dietrich Featherston <d@d2fn.com>
2025-12-04 11:40:13 -0800

    add nix module

---
+++ nixos/hm/nix-lsp.nix
@@ -0,0 +1,8 @@
+{ config, pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    nil
+    nixfmt-rfc-style
+  ];
+}
--- nixos/home/hm/default.nix
+++ nixos/home/hm/default.nix
@@ -1,42 +1,42 @@
 { config, pkgs, ... }:
 
 {
-	programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 
-	home = {
-		username = "d";
-		homeDirectory = "/home/d";
-		stateVersion = "25.05";
-		sessionVariables = {
-			QT_QPA_PLATFORM = "wayland";
-			OBSIDIAN_REST_API_KEY = "890170f6ef90e5c9cd2be20746cb55d2a84e43b23467f54ed2005f25e79f5df2";
-		};
-		packages = with pkgs; [
-			mpv
-			celluloid
-			veracrypt
-			digikam
-			mpd
-			ncmpcpp
-		];
-	};
+  home = {
+    username = "d";
+    homeDirectory = "/home/d";
+    stateVersion = "25.05";
+    sessionVariables = {
+      QT_QPA_PLATFORM = "wayland";
+      OBSIDIAN_REST_API_KEY = "890170f6ef90e5c9cd2be20746cb55d2a84e43b23467f54ed2005f25e79f5df2";
+    };
+    packages = with pkgs; [
+      mpv
+      celluloid
+      veracrypt
+      digikam
+      mpd
+      ncmpcpp
+    ];
+  };
 
-	services.mpd = {
-		enable = true;
-		musicDirectory = "~/Dropbox/music";
-		extraConfig = ''
-			audio_output {
-				type "pulse"
-				name "Pulse Output"
-			}
-		'';
-	};
+  services.mpd = {
+    enable = true;
+    musicDirectory = "~/Dropbox/music";
+    extraConfig = ''
+      			audio_output {
+      				type "pulse"
+      				name "Pulse Output"
+      			}
+      		'';
+  };
 
-	xdg.configFile."ncmpcpp/config".text = ''
-		ncmpcpp_directory = "~/.config/ncmpcpp"
-		mpd_host = "localhost"
-		mpd_port = "6600"
-	'';
+  xdg.configFile."ncmpcpp/config".text = ''
+    		ncmpcpp_directory = "~/.config/ncmpcpp"
+    		mpd_host = "localhost"
+    		mpd_port = "6600"
+    	'';
 
   # udiskie is user-level automounter
   services.udiskie = {
@@ -47,14 +47,15 @@
     tray = "auto";
   };
 
-	imports = [
-		../../hm/gtk.nix
-		../../modules/focus_or_launch.nix
-		./digikam.nix
-		./dropbox.nix
-		./hyprland.nix
-		./waybar.nix
-		./electronics.nix
-	];
+  imports = [
+    ../../hm/gtk.nix
+    ../../hm/nix-lsp.nix
+    ../../modules/focus_or_launch.nix
+    ./digikam.nix
+    ./dropbox.nix
+    ./electronics.nix
+    ./hyprland.nix
+    ./waybar.nix
+  ];
 
 }
